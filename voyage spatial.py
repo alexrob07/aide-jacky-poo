d97339f58b7608e2aa7883aa00ae4a3887d3e028
Alexandre <gizmo2.ar@gmail.com>
2025-09-06 22:37:40 -0400

    fini

--- voyage spatial.py
+++ voyage spatial.py
@@ -3,19 +3,19 @@ import random
 masse_astronautes = 80
 module_spatiale = 500
 
-def astronaute(masse,nb_astronautes):
+def astronaute(masse:int,nb_astronautes:int):
     masse_total_astronautes = nb_astronautes * masse
-    return masse_total_astronautes,nb_astronautes
+    return masse_total_astronautes
 
-def module(masse,nb_module):
+def module(masse:int,nb_module:int):
     masse_total_module = nb_module * masse
     return masse_total_module
 
-def carburant_avant(masse):
+def carburant_avant(masse:int):
     carburant = masse * 2
     return carburant
 
-def masse_finale(masse1, masse2):
+def masse_finale(masse1:int, masse2:int):
     masse_fin =  masse1 + masse2
     return masse_fin
 
@@ -30,7 +30,7 @@ def vip(carburant:int):
         print("vous etes pas eligible")
         return carburant
 
-def tempete_solaire(tempete):
+def tempete_solaire(tempete:str):
     nombre = random.randint(1,2)
     if nombre == 1:
         tempete = "interdit (tempete solaire)"
@@ -45,6 +45,7 @@ if __name__ == "__main__":
     nom_mission = input("quel est le nom de la mission?: ").upper()
     nombre_astronautes = int(input("Combien d'astronautes y-a-t-il?: "))
     nombre_module = int(input("Combien de modules y-a-t-il?: "))
+
     masse_final_astronautes = astronaute(masse_astronautes, nombre_astronautes)
     masse_final_module = module(module_spatiale,nombre_module)
 
@@ -52,25 +53,26 @@ if __name__ == "__main__":
 
     carburant_initial = carburant_avant(masse_total)
     carburant_final = vip(carburant_initial)
-    tempete = ""
-    tempete_solaire(tempete)
+    status_tempete = ""
+    nouveau_status = tempete_solaire(status_tempete)
     mission = "Non-realisable (carburant insufisant)"
 
-    if carburant_final == masse_total *2:
+    if carburant_final >= masse_total *2:
         mission = "Realisable (carburant sufisant)"
 
 
     print(
-        f"================================================"
-        f"RAPPORT DE MISSION : {nom_mission}"
-        f"Nombres d'astronautes : {nombre_astronautes}(masse totale{masse_final_astronautes} Kg) "
-        f"Modules disponibles : {nombre_module}((masse totale {masse_final_module} kg)) "
-        f"Masse totale du vaisseau : {masse_total} Kg"
-        f"Carburant nécessaire : {carburant_initial}L"
-        f"Mission VIP, bonus VIP applicable"
-        f"  Carburant après bonus VIP : {carburant_final}L"
-        f"Mission {mission}"
-        f"Depart {tempete} "
+        f"================================================\n"
+        f"RAPPORT DE MISSION : {nom_mission}\n"
+        f"Nombres d'astronautes : {nombre_astronautes} (masse totale {masse_final_astronautes} Kg)\n"
+        f"Modules disponibles : {nombre_module} (masse totale {masse_final_module} kg)\n"
+        f"Masse totale du vaisseau : {masse_total} Kg\n"
+        f"Carburant nécessaire : {carburant_initial} L\n"
+        f"Mission VIP, bonus VIP applicable\n"
+        f"  Carburant après bonus VIP : {carburant_final} L\n"
+        f"Mission {mission}\n"
+        f"Depart {nouveau_status}\n "
+        f"================================================\n"
     )
 
 
